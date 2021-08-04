05cd1876a4320e2179eba1b9fc1b4345f29c1a5d
anhquoc1301 <anhquoc1301@gmail.com>
2021-08-04 19:34:35 +0700

    Initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,46 @@
+using System.Collections;//adsgfjka;ư
+
+using System.Collections.Generic;
+using UnityEngine;//adsgfjka;ư
+
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [FormerlySerializedAs("hgjdwe")] [FormerlySerializedAs("ygdjuywed")] [FormerlySerializedAs("kjhfkjwe")] [FormerlySerializedAs("hfikuf")] [FormerlySerializedAs("gfhd")] [FormerlySerializedAs("jfiklf")] [FormerlySerializedAs("duongbt")] [FormerlySerializedAs("khdjw")] [FormerlySerializedAs("efherjkfhr")] [FormerlySerializedAs("hjdsed")] [FormerlySerializedAs("yughud")] [FormerlySerializedAs("coing2")] public Text jhbfjhsd;
+
+    
+      [FormerlySerializedAs("jkhdkjwe")] [FormerlySerializedAs("kjhkfe")] [FormerlySerializedAs("hgjjfe")] [FormerlySerializedAs("ghsjde")] [FormerlySerializedAs("jhfcjws")] [FormerlySerializedAs("shgfdtywe")] [FormerlySerializedAs("jhbdfjchcer")] [FormerlySerializedAs("freferkfj")] [FormerlySerializedAs("kjhkfjd")] [FormerlySerializedAs("hjdj")] [FormerlySerializedAs("kjhdwd")] public GameObject jhduse;//adsggjfhjherffjka;ư
+
+
+     public static GameController jgfhjsd;
+    
+     [FormerlySerializedAs("jfhweuijf")] [FormerlySerializedAs("jhfkjse")] [FormerlySerializedAs("kjhkfjer")] [FormerlySerializedAs("hjkhdjwe")] [FormerlySerializedAs("jhkf")] [FormerlySerializedAs("loser")] [FormerlySerializedAs("jhgdgh")] [FormerlySerializedAs("jygduywe")] [FormerlySerializedAs("hkujcher")] [FormerlySerializedAs("nkjcwef")] [FormerlySerializedAs("jhdu")] [FormerlySerializedAs("bjhbdfj")] public int jgdjhsed;//adsgfjka;ư
+
+//dfgvhjkjerfrlkgj
+//kjshfkjer
+//fksjdnfkje
+//kfdbdjskfb
+//fswhekfhnekwr
+//vtfdkhgkjhrtgkirt
+
+    private void Awake()//adsgfjk
+
+    {
+        jgfhjsd = this;
+    }
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jgdjhsed = PlayerPrefs.GetInt("Coin", 0);
+        jhbfjhsd.text = jhbfjhsd.ToString();//adsgfjka;ư
+//adsgfjka;ư
+
+    }
+}
+//adsgfjka;ư//adsgfjka;ư
+//ad
